966c3d83b3570a296e9bc426f5d5f624d9839889
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2025-11-07 22:37:50 +0500

    Taklif havolasi tayyor bo'ldi

--- routes/userRouter.py
+++ routes/userRouter.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F 
 from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
 from aiogram.fsm.context import FSMContext
-
+from aiogram.enums import ParseMode
 
 from states.userState import TestAnswersFromUser, UserActions
 
@@ -10,6 +10,21 @@ from states.userState import TestAnswersFromUser, UserActions
 ###########################################################################################################
 
 user = Router()
+
+@user.message(F.text == "Taklif havolam")
+async def taklifHavolasi(message: Message):
+    tg_id = message.from_user.id
+    text = f"""Sizning taklif havolangiz:
+    "Ushbu taklif havolani boshqalarga yuborishingiz mumkin: 
+    https://t.me/darslik1202_bot?start={tg_id}"
+
+    Bu havolani guruh va chatlarda tarqating va ballarga sazovor bo'ling
+    """
+    
+    # Matnni yuborish
+    await message.answer(text=text)
+
+
 
 @user.message(UserActions.work_test)
 async def userActions_work_test(message: Message, state: FSMContext):
@@ -28,11 +43,6 @@ async def testAnswerfromUser_test_code(message: Message, state: FSMContext):
 async def testAnswerfromUser_answers(message: Message, state: FSMContext):
     pass
     # user test kalitlarini kiritish jarayoni
-
-@user.message(TestAnswersFromUser.test_code)
-async def testAnswerfromUser_test_code(message: Message, state: FSMContext):
-    pass
-    # user test ishlash jarayoni test kodini so'rash jarayoni
     
 @user.message(TestAnswersFromUser.tekshiruv)
 async def testAnswerfromUser_tekshiruv(message: Message, state: FSMContext):
